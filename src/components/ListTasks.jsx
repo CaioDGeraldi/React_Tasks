78e01382ec546c981bee6013b8d99e2e49e2c9c1
CaioDGeraldi <chdgeraldi@gmail.com>
2026-09-03 12:03:33 -0300

    chore(arquitetura): definir arquitetura e componentes

---
+++ src/components/ListTasks.jsx
@@ -0,0 +1,14 @@
+function ListTasks() {
+    return (
+        <div className="w-full h-full flex items-center justify-center py-10">
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export { ListTasks }
