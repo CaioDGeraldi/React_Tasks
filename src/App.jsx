a36b4a76c5590cd4ae785c7fe373e73b988b0699
CaioDGeraldi <161092309+CaioDGeraldi@users.noreply.github.com>
2026-09-04 07:34:17 -0300

    chore(arquitetura): definir arquitetura e componentes (#1)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ListTasks } from './components/ListTasks'
+import { AddTasks } from './components/AddTasks'
+
+function App() {
+
+  return (
+  <>
+    <AddTasks />
+    <ListTasks />
+  </>  
+  
+  )
+}
+
+export { App }
---
+++ src/components/AddTasks.jsx
@@ -0,0 +1,12 @@
+function AddTasks() {
+    return (
+        <div className="w-full h-full flex items-center justify-center py-10">
+            <input>
+            
+            </input>
+        </div>
+    )
+}
+
+
+export { AddTasks }
